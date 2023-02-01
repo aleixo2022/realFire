72983f0cb238cfeeaabd469f43a2dca23a81c0dc
alexsandro t. nascimento <alexsandro.nascimento@newport.com.br>
2023-02-01 18:00:31 -0300

    atualizado dados

--- compare_data_mov.py
+++ compare_data_mov.py
@@ -6,6 +6,12 @@ pqmData = pqmQuery.consulta()
 
 fqmData = fqmQuery.consulta()
 
-result = pd.DataFrame(pqmData, columns=['mov_estoque', 'produto',  'filial', 'empenho', 'quantidade','idlocal','tipo_origem','data'])
+dfmov1 = pd.DataFrame(fqmData, columns=['mov_estoque', 'produto',  'filial', 'empenho', 'quantidade','idlocal','tipo_origem','data'])
+dfmov2 = pd.DataFrame(pqmData, columns=['mov_estoque', 'produto',  'filial', 'empenho', 'quantidade','idlocal','tipo_origem','data'])
 
-print(result)
+not_in_dfmov2 = dfmov1[~dfmov1['mov_estoque'].isin(dfmov2['mov_estoque'])]
+not_in_dfmov2.to_csv("base_inexistente.csv", index=False)
+dfmov1.to_csv("dfmov1.csv", index=False)
+dfmov2.to_csv("dfmov2.csv", index=False)
+
+print(not_in_dfmov2)
